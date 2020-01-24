437b541371d8040c1102ea6be30a8da305940c03
Klemen Kenda <klemen.kenda@ijs.si>
2020-01-24 13:10:06 +0100

    Upgrading basic crawler functionality

---
+++ src/crawler/arso-groundwater-slovenia/crawler.js
@@ -0,0 +1,45 @@
+// imports
+const CrawlerUtils = require('../crawlerutils');
+
+export default class ArsoGroundwaterSloveniaCrawler {
+
+    /**
+     * Responsible for loading state
+     */
+    constructor() {
+        // loading config
+        this.config = CrawlerUtils.loadConfig(__dirname);
+        // loading state
+        this.state = CrawlerUtils.loadState(__dirname);
+    }
+
+    /**
+     * Responsible for crawling at this step.
+     *
+     * NOTE: Take care that the crawler is resistant to delays (it crawls
+     * also historic data if needed). Crawler should do everything that
+     * is needed.
+     */
+    crawl() {
+        // do the crawling here
+
+        // update datalake repository with the crawled data
+
+        // update the state with the last crawled timestamp
+
+        // update state
+        state.lastts = new Date("2020-01-01T10:10:10Z").getTime();
+
+        // write final state
+        CrawlerUtils.saveState(__dirname, state);
+
+    }
+
+    /**
+     * Loads the datalake data into the database.
+     */
+    load() {
+        // load the data from datalake into the db
+    }
+
+}
