ccfd4c9c925a180627c6d3177cb13e7d989c2099
Klemen Kenda <klemen.kenda@ijs.si>
2020-03-11 15:11:54 +0100

    Cleanup and sensor_type/node config definitions

--- src/crawler/index.js
+++ src/crawler/index.js
@@ -1,35 +1,31 @@
-<<<<<<< HEAD
 // imports
 const CrawlerUtils = require('./crawlerutils');
-
-// dynamically load all the crawlers
-let crawler_configs = CrawlerUtils.getCrawlers();
-
-let Crawlers = [];
-let crawlers = [];
-
-crawler_configs.forEach((crawler, i) => {
-
-    Crawlers.push(require(crawler.dir + "/crawler.js"));
-    crawlers.push(new Crawlers[i]());
-
-});
-
-crawlers[1].crawl();
-=======
-// imports
-const CrawlerUtils = require('./crawlerutils');
+const ActiveCrawlers = require('./active_crawlers.json');
+const fs = require('fs');
 
 // dynamically load all the crawlers
 const crawlerConfigs = CrawlerUtils.getCrawlers();
 
 const Crawlers = [];
 const crawlers = [];
+j = 0
 
 crawlerConfigs.forEach((crawler, i) => {
-    Crawlers.push(require(crawler.dir + '/crawler.js'));
-    crawlers.push(new Crawlers[i]());
+    if (ActiveCrawlers.includes(crawler.config.id)) {
+        Crawlers.push(require(crawler.dir + '/crawler.js'));
+        crawlers.push(new Crawlers[j]());
+
+        Crawlers.push(require(crawler.dir + '/crawler.js'));
+        crawlers.push(new Crawlers[j]());
+
+        if (!fs.existsSync(__dirname + '/../data/' + crawler.config.id)) {
+            fs.mkdirSync(__dirname + '/../data/' + crawler.config.id);
+        }
+
+        j++;
+    }
 });
 
-crawlers[1].crawl();
->>>>>>> 9b477b654450cdf3f7a648e590d5f077acddabf0
+crawlers.forEach((crawler) => {
+    crawler.crawl();
+});
