2794323c10ad64a060166c33d302efba8e211032
Klemen Kenda <klemen.kenda@ijs.si>
2020-03-27 15:04:15 +0100

    Crawler to PG

--- src/crawler/utils/sqlutils.js
+++ src/crawler/utils/sqlutils.js
@@ -1,5 +1,6 @@
 // includes
-let mariadb = require('mariadb');
+const mariadb = require('mariadb');
+const { Pool } = reqire('pg');
 
 /**
  * A class for different utils for working with crawlers in
@@ -8,9 +9,19 @@ let mariadb = require('mariadb');
 class SQLUtils {
 
     constructor(config) {
-        this.config = require('../../common/config.json')['storage']['rapids-iot'];
+        this.config = require('../../common/config.json')['storage']['rapids-iot-pg'];
         this.pool = null;
-        this.connectToMariaDB();
+        // this.connectToMariaDB();
+        this.connectToPg();
+    }
+
+    connectToPg() {
+        this.pool = new Pool({
+            host: this.config.host,
+            user: this.config.user,
+            password: this.config.password,
+            database: this.config.db,
+        })
     }
 
     connectToMariaDB() {
@@ -29,17 +40,10 @@ class SQLUtils {
     // -------------------------------------------------------
 
     async getPlaces() {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-
             // retrieve places
-            const sql = `select * from places`;
-            let data = await conn.query(sql);
+            const sql = `select * from rapids_iot.places`;
+            let data = await pool.query(sql);
 
             // delete unnecessary data
             delete data.meta;
@@ -47,28 +51,18 @@ class SQLUtils {
         } catch(e) {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
-            this.connectToMariaDB();
-        } finally {
-            if (conn) conn.release();
         }
     }
 
     async insertPlace(crawler, uuid, title, x, y) {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-
             // sql
             const sql = `
-                insert into places (crawler, uuid, title, x, y)
+                insert into rapids_iot.places (crawler, uuid, title, x, y)
                 values('${crawler}', '${uuid}', '${title}', ${x}, ${y})
             `;
 
-            conn.query(sql);
+            this.pool.query(sql);
             console.log("Inserted place: " + uuid);
 
             return true;
@@ -76,8 +70,6 @@ class SQLUtils {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
             return false;
-        } finally {
-            if (conn) conn.release();
         }
     }
 
@@ -87,17 +79,10 @@ class SQLUtils {
     // -------------------------------------------------------
 
     async getNodes() {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-
             // retrieve places
-            const sql = `select * from nodes`;
-            let data = await conn.query(sql);
+            const sql = `select * from rapids_iot.nodes`;
+            let data = await this.pool.query(sql);
 
             // delete unnecessary data
             delete data.meta;
@@ -105,28 +90,18 @@ class SQLUtils {
         } catch(e) {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
-            this.connectToMariaDB();
-        } finally {
-            if (conn) conn.release();
-        }
+        };
     }
 
     async insertNode(uuid, place_uuid, title) {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-
             // sql
             const sql = `
-                insert into nodes (uuid, place_uuid, title)
+                insert into rapids_iot.nodes (uuid, place_uuid, title)
                 values('${uuid}', '${place_uuid}', '${title}')
             `;
 
-            conn.query(sql);
+            this.pool.query(sql);
             console.log("Inserted node: " + uuid);
 
             return true;
@@ -134,8 +109,6 @@ class SQLUtils {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
             return false;
-        } finally {
-            if (conn) conn.release();
         }
     }
 
@@ -145,17 +118,10 @@ class SQLUtils {
     // -------------------------------------------------------
 
     async getSensors() {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-
             // retrieve places
-            const sql = `select * from sensors`;
-            let data = await conn.query(sql);
+            const sql = `select * from rapids_iot.sensors`;
+            let data = await this.pool.query(sql);
 
             // delete unnecessary data
             delete data.meta;
@@ -163,28 +129,18 @@ class SQLUtils {
         } catch(e) {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
-            this.connectToMariaDB();
-        } finally {
-            if (conn) conn.release();
         }
     }
 
     async insertSensor(uuid, node_uuid, sensor_type_uuid, title) {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-
             // sql
             const sql = `
-                insert into sensors (uuid, node_uuid, sensor_type_uuid, title)
+                insert into rapids_iot.sensors (uuid, node_uuid, sensor_type_uuid, title)
                 values('${uuid}', '${node_uuid}', '${sensor_type_uuid}', '${title}')
             `;
 
-            conn.query(sql);
+            this.pool.query(sql);
             console.log("Inserted sensor: " + uuid);
 
             return true;
@@ -192,9 +148,7 @@ class SQLUtils {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
             return false;
-        } finally {
-            if (conn) conn.release();
-        }
+        };
     }
 
 
@@ -203,34 +157,23 @@ class SQLUtils {
     // -------------------------------------------------------
 
     async insertMeasurement(sensor_id, ts, value) {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-
             // sql
             const sql = `
                 insert into measurements (sensor_id, ts, value)
                 values(${sensor_id}, FROM_UNIXTIME(${ts}), ${value})
             `;
-
-            conn.query(sql);
-
+            this.pool.query(sql);
             return true;
         } catch (e) {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
             return false;
-        } finally {
-            if (conn) conn.release();
         }
     }
 
     insertMeasurementSQL(sensor_id, ts, value) {
-        return `insert into measurements (sensor_id, measurement_ts, value) values(${sensor_id}, FROM_UNIXTIME(${ts}), ${value}); `;
+        return `insert into rapids_iot.measurements (sensor_id, measurement_ts, value) values(${sensor_id}, FROM_UNIXTIME(${ts}), ${value}); `;
     }
 
 
@@ -239,22 +182,13 @@ class SQLUtils {
     // -------------------------------------------------------
 
     async processSQL(sql) {
-        let conn;
-
         try {
-            // create connection
-            conn = await this.pool.getConnection();
-            // select appropriate database
-            conn.query(`use rapidsiot;`);
-            conn.query(sql);
-
+            this.pool.query(sql);
             return true;
         } catch (e) {
             // display error and assume the link is down - repeat connecting to DB
             console.log("Error", e);
             return false;
-        } finally {
-            if (conn) conn.release();
         }
     }
 
--- src/storage/rapids-iot-pg/data.1.sql
+++ src/storage/rapids-iot-pg/data.1.sql
@@ -1,5 +1,5 @@
 -- data
-insert into sensor_types (uuid, phenomena, uom, description) values ('gap', 'gap', 'm', 'gap between cars');
-insert into sensor_types (uuid, phenomena, uom, description) values ('speed', 'speed', 'km/h', 'average speed');
-insert into sensor_types (uuid, phenomena, uom, description) values ('count', 'count', '', 'number cars per hour');
-insert into sensor_types (uuid, phenomena, uom, description) values ('class', 'class', '', 'class label');
+insert into rapids_iot.sensor_types (uuid, phenomena, uom, description) values ('gap', 'gap', 'm', 'gap between cars');
+insert into rapids_iot.sensor_types (uuid, phenomena, uom, description) values ('speed', 'speed', 'km/h', 'average speed');
+insert into rapids_iot.sensor_types (uuid, phenomena, uom, description) values ('count', 'count', '', 'number cars per hour');
+insert into rapids_iot.sensor_types (uuid, phenomena, uom, description) values ('class', 'class', '', 'class label');
