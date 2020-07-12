9c8abb8def97f1e467c295c133efd42c4a08e8c0
unknown <jaka.skerl@gmail.com>
2020-07-12 20:23:19 +0200

    Updating the daily weather crawler

--- src/crawler/crawlers/arso_daily_weather/crawler.js
+++ src/crawler/crawlers/arso_daily_weather/crawler.js
@@ -27,36 +27,40 @@ class TemplateCrawler {
      * also historic data if needed). Crawler should provide all the steps
      * denoted in the comments of this method.
      */
-    crawl() {
+    async crawl() {
         console.log('Starting crawl: ' + this.config.id);
         // do the crawling here
-        let logger = this.getData()
-
-
+        let weatherData = await this.getData()
+        
 
 
         // update datalake repository with the crawled data
-
         // update the state with the last crawled timestamp
+        await this.dataLake(weatherData);
 
-        // update state
+
+
 
         // write final state
-        //CrawlerUtils.saveState(__dirname, state);
+        CrawlerUtils.saveState(__dirname, this.state);
 
         console.log('Finishing crawl: ' + this.config.id);
     }
 
     async getData(url = this.config.start) {
+
+        let weatherData = []
+
         const body = await CrawlerUtils.getURL(url);
         let rmeasurements = [];
         let rheads = [];
+        let rstyleHtml = [];
         const $ = cheerio.load(body);
         
         let data = $(".meteoSI-table tbody tr td").each((i, el) => {
             let text = $(el).text()
             rmeasurements.push(text);
-            console.log($(el).html)
+            rstyleHtml.push($(el).html());
 
         });
         let head = $(".meteoSI-table thead tr th").each((i, el) => {
@@ -70,8 +74,12 @@ class TemplateCrawler {
         let heads = [];
         while(rheads.length) heads.push(rheads.splice(0,11));
 
+        let styleHtml = [];
+        while(rstyleHtml.length) styleHtml.push(rstyleHtml.splice(0,11));
 
+        let i = 0;
         for (let station of measurements) {
+            
             let stationData = {};
             for (let varIdx in station) {
                 
@@ -81,16 +89,52 @@ class TemplateCrawler {
                     stationData["Postaja"] = station[varIdx];
                     stationData["Datum"] = heads[0][varIdx];
                 
+                }
+                
+                if (styleHtml[i][varIdx].includes("/uploads/meteo/style/img")) {
 
+                       
+                    stationData[heads[0][varIdx]] = styleHtml[i][varIdx].slice(43, -6)
                 }
             }
-            
+            i++;
 
-            //console.log(stationData)
-        
+
+            weatherData.push(stationData);
         }
+    
+        return weatherData;
+        
         
     };
+
+    async dataLake(data) {
+        for (let station of data) {
+            let time = station.Datum.split(", ")[1].slice(0,-5)
+            station.Datum = moment(time, "DD-MM-YYYY HH:mm").toISOString(true);
+            let unix = moment(station.Datum).unix();
+            let lastUnix;
+            
+
+            if (this.state != {}) {
+                if (this.state[station.Postaja] !== undefined) {
+                    lastUnix = this.state[station.Postaja].lastRecord;
+                } else {
+                    this.state[station.Postaja] = {"lastRecord" : unix};
+                }
+            }
+            if (lastUnix !== unix) {
+                CrawlerUtils.saveToDataLake(JSON.stringify(station), time, {
+                    dir: this.config.id,
+                    type: this.config.log_type,
+                    name: station.Postaja,
+                })
+            }
+            
+        }
+    }
+    
+
 
     /**
      * Loads the datalake data into the database.
