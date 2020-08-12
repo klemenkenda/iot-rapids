5794fa9f817d1ea428cb80fd1f4e1e145c64a4e8
Gregor <gregor.krzmanc@gmail.com>
2020-08-12 15:47:55 +0200

    Add functions for JSONStat processing

--- src/crawler/test/crawlerutils_test.js
+++ src/crawler/test/crawlerutils_test.js
@@ -6,4 +6,17 @@ describe('Testing crawlerutils', () => {
         const crawlers = CrawlerUtils.getCrawlers();
         assert.equal(Array.isArray(crawlers), true);
     });
+
+    it('buildJSONStatQuery', async () => {
+        const testUrl = "https://pxweb.stat.si:443/SiStatDb/api/v1/sl/20_Ekonomsko/14_poslovni_subjekti/01_14188_podjetja/1418807S.px";
+        const testRestrictions = {
+            "LETO": ["2012"],
+            "OBČINE": ["002"]
+        }
+        const query = await CrawlerUtils.buildJSONStatQuery(testUrl, testRestrictions);
+        
+        assert.equal(query.query === undefined, false);
+        assert.equal(query.query.length > 0, true);
+
+    });
 });
